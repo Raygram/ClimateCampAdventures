309b02e67e8aa0132b2188d9b437589177e20e48
Marlena <marlnapp59@gmail.com>
2022-06-07 13:08:07 +0200

    Add TODO

---
+++ Assets/Scripts/TODO.cs
@@ -0,0 +1,28 @@
+/*
+Task + Priority (1 = high, 5 = low)
+Player Actions:
+über bach springen
+zäune aufschneiden mit zange 1
+bagger besetzen 1
+baumhaus bauen
+
+stier/bulle
+
+optional:
+maleranzug
+an bagger anketten
+tripod bauen
+
+Environment:
+Waldgelände 1 
+Grube mit Bagger, Krahn etc 1
+ende gelände rufe als hintergrund musik 2
+banner 2
+pyro 4
+kohle fließband 4
+stier/bulle 3
+
+Task Manager: 1
+Task 1: ...
+Task 2: ...
+*/
