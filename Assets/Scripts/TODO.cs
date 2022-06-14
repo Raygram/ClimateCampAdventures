e55e79dae86dba9a2709eb3f786a35546c49b8e0
Marlena <marlnapp59@gmail.com>
2022-06-14 13:39:24 +0200

    update TODO

--- Assets/Scripts/TODO.cs
+++ Assets/Scripts/TODO.cs
@@ -5,8 +5,11 @@ Player Actions:
 zäune aufschneiden mit zange 1 Jona
 bagger besetzen 1
 baumhaus bauen
-correct camera movement (marlena)
-animate digger
+correct camera movement (marlena) check
+even out path  (marlena) check
+animate digger (marlena)
+add förderband (marlena)
+create river (marlena)
 
 stier/bulle
 
@@ -15,18 +18,19 @@ an bagger anketten
 tripod bauen
 
 Environment:
-Waldgelände 1 
-Grube mit Bagger, Krahn etc 1
+Waldgelände 1 check
+Grube mit Bagger, Krahn etc 1 check
 ende gelände rufe als hintergrund musik 2
 banner 2
-zelt
+zelt check
 pyro 4
 kohle fließband 4
 stier/bulle NPC 5
 
 Task Manager: 1 Bogdan
-Task 1: Finde den Eingang zur Kohlegrube - Schneide den Zaun auf 
-Task 2: finde den bagger und stoppe ihn (bagger als interactable,
+Task 1 springe über den fluss um den eingang zu kohlegrube zu finden
+Task 2: Finde den Eingang zur Kohlegrube - Schneide den Zaun auf 
+Task 3: finde den bagger und stoppe ihn (bagger als interactable,
 wenn interaction/collision mit bagger --> bagger freezes)
 
 
