8286cc291d39d8ad3632c8545864adf0a5855505
Marlena <marlnapp59@gmail.com>
2022-06-22 15:11:13 +0200

    add digger Collider

---
+++ Assets/Scripts/DiggerCollider.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiggerCollider : MonoBehaviour
+{
+    public bool touchDigger;
+
+    public bool GetCompleted()
+    {
+        return touchDigger;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        touchDigger = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            touchDigger = true;
+            this.gameObject.SetActive(false);
+        }
+    
+    }
+}
